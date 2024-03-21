3f50d01dea695e4699c19d3c6c8a182fda761ee7
Felipe Specht <felipespecht@hotmail.com>
2024-03-21 15:46:18 -0300

    Fixed some bugs on the tags and next version

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,51 +1,53 @@
 export default function Experience() {
     return (
-        <section id="experienceSection" className="bg-white dark:bg-gray-900">
-          	<div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 md:py-16 lg:px-8">
-            	<div className="mx-auto max-w-3xl text-center">
-					<h2 className="text-3xl font-bold text-gray-900 dark:text-white sm:text-4xl">
-						Experience
-					</h2>
+		<>
+			<section id="experienceSection" className="bg-white dark:bg-gray-900">
+				<div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 md:py-16 lg:px-8">
+					<div className="mx-auto max-w-3xl text-center">
+						<h2 className="text-3xl font-bold text-gray-900 dark:text-white sm:text-4xl">
+							Experience
+						</h2>
 
-					<p className="mt-4 text-gray-500 dark:text-gray-400 sm:text-xl">
-						Below are some important metrics that I achieved in the few past years.
-              		</p>
-            	</div>
+						<p className="mt-4 text-gray-500 dark:text-gray-400 sm:text-xl">
+							Below are some important metrics that I achieved in the few past years.
+						</p>
+					</div>
 
-            	<div className="mt-8 sm:mt-12">
-              		<dl className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-                		<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Working on IT</dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">5 Years</dd>
-                		</div>
+					<div className="mt-8 sm:mt-12">
+						<dl className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Working on IT</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">5 Years</dd>
+							</div>
 
-                		<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Vanilla JavaScript</dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">2 Years</dd>
-                		</div>
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Vanilla JavaScript</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">2 Years</dd>
+							</div>
 
-						<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Python</dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">3 Years</dd>
-                		</div>
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Python</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">3 Years</dd>
+							</div>
 
-                		<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">C++ & C#</dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">proficiency</dd>
-                		</div>
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">C++ & C#</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">proficiency</dd>
+							</div>
 
-						<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Javascript Frameworks:<br>Node.js; React.js; Next.js</br></dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">proficiency</dd>
-                		</div>
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">Javascript Frameworks:<br />Node.js; React.js; Next.js</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">proficiency</dd>
+							</div>
 
-						<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
-                  			<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">My coffee taste</dt>
-                  			<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">Very Good</dd>
-                		</div>
-              		</dl>
-            	</div>
-          	</div>
-        </section>
+							<div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center bg-gray-800 dark:border-gray-700">
+								<dt className="order-last text-lg font-medium text-gray-500 dark:text-gray-400">My coffee taste</dt>
+								<dd className="text-4xl font-extrabold text-cyan-400 md:text-5xl">Very Good</dd>
+							</div>
+						</dl>
+					</div>
+				</div>
+			</section>
+		</>
     )
 }
